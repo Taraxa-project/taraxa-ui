9d6b96d7161964b58efba9d775d5f85a2de3abd9
DMachado93 <66080295+DMachado93@users.noreply.github.com>
2021-07-27 13:47:53 +0100

    First component with Storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
+import Button, { ButtonProps } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = { label: "Primary", size: "large" };
+
+export const Secondary = Template.bind({});
+Secondary.args = { ...Primary.args, primary: false, label: "Secondary" };
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./button.css";
+
+export interface ButtonProps  {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: "small" | "medium" | "large";
+  label: string;
+  onClick?: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+};
+
+const Button = ({
+  primary = true,
+  backgroundColor,
+  size = "medium",
+  onClick,
+  label,
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={backgroundColor ? { backgroundColor }: {}}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
